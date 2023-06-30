86ea9f650b26bac4eae56861bb198a6ce3cd1f45
TusharRB <91899841+TusharRB@users.noreply.github.com>
2023-06-30 13:02:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
+	const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+
+  alert(`First Name: ${firstName}\nLast Name: ${lastName}`);
 
 }
+
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  getFormValue();
+});
